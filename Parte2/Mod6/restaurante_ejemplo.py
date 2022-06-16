59d92f796bdd7b428b1a78d348346a031c3a2aac
jcorredorc <javier.adolfo@gmail.com>
2022-06-16 08:56:54 -0500

    feat: restaurant example

--- Parte2/Mod6/restaurante_ejemplo.py
+++ Parte2/Mod6/restaurante_ejemplo.py
@@ -1,27 +1,26 @@
 import datetime as dt
 
-
 class restaurante():
-    def __init__(self, nombre, mesas):
+    def __init__(self, nombre, mesas, menu):
         self.name = nombre
-        self.menu = ['arroz', 'papa', 'carne', 'vedura', 'jugo']
         self.num_tables = mesas
+        self.menu = menu
 
     def msg_bienvenida(self):
         ahora = dt.datetime.now()
         # print(dt.datetime.now().hour)
         if ahora.hour <= 12:
             print("Buenos días ", self.name)
-        elif 12 <= ahora.hour <= 18:
+        elif 12 < ahora.hour <= 18:
             print("Buenas tardes ", self.name)
         else:
             print("Buenas noches ", self.name)
 
-    def msg_horario(slef):
-        print("El horario de atención es de 11AM a 3 PM L- V ")
+    def msg_horario(self):
+        print("El horario de atención en ",self.name," es de 11AM a 3 PM L- V ")
 
 
-el_recuerdo = restaurante("Recuerdo", 5)
+el_recuerdo = restaurante("Recuerdo", 5, ['arroz', 'papa', 'carne', 'vedura', 'jugo'])
 print(el_recuerdo.menu)
 print(el_recuerdo.name)
 print(el_recuerdo.num_tables)
